6667431c27b604e2c45dd9003eaa8b7710ca187e
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-12 13:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+        displayOutput = 'original  : ' +touristSpots  ;
 
+        var sortedArr = touristSpots.sort();
+        displayOutput += '\nSorted : ' + sortedArr;
+
+        document.getElementById('output').innerHTML = displayOutput;
